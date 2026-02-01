feeda323fdc83d2a5abd7df4cb475439a72a7b20
teamcutter <ruslan.sibgatulin05@gmail.com>
2026-02-01 21:31:26 +0500

    refactor: re-arrange extensions from most to lowest popular

--- internal/domain/constants.go
+++ internal/domain/constants.go
@@ -1,6 +1,6 @@
 package domain
 
-var extensions = []string{".tar.zst", ".tar.gz", ".tar.xz", ".tar.bz2", ".tgz", ".txz", ".tzst"}
+var extensions = []string{".tar.gz", ".tar.zst", ".tar.xz", ".tar.bz2", ".tgz", ".txz", ".tzst"}
 
 func Extensions() []string {
 	return append([]string{}, extensions...)
