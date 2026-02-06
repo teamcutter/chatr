15d776a76f6959927e0a002f1693da2316249f0b
teamcutter <ruslan.sibgatulin05@gmail.com>
2026-02-07 00:28:58 +0500

    fmt: missed -

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -13,8 +13,8 @@ func newVersionCmd() *cobra.Command {
 		Use:   "version",
 		Short: "Print the version of chatr",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("%s%s%s%s%s%s\n", bold("chatr"), bold("-"), bold(version.Version),
-				bold(runtime.GOOS), bold("/"), bold(runtime.GOARCH))
+			fmt.Printf("%s%s%s%s%s%s%s\n", bold("chatr"), bold("-"), bold(version.Version),
+				bold("-"), bold(runtime.GOOS), bold("/"), bold(runtime.GOARCH))
 		},
 	}
 }
